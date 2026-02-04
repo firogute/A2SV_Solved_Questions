c5c6cfc630021981b40876a7b588625313ff1f17
Firomsa Guteta <154022111+firogute@users.noreply.github.com>
2026-02-04 15:42:08 +0300

    Time: 5 ms (71.59%), Space: 19.4 MB (28.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        str_x = str(x)
+
+        rev_x = str_x[::-1]
+
+        return str_x == rev_x

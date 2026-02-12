bfbfa527c329580b2ccf2932d7c2469155051311
Firomsa Guteta <firomsaguteta11@gmail.com>
2026-02-12 22:18:31 +0300

    add implementation of setZeroes function to modify a matrix in-place by setting entire rows and columns to zeroes

---
+++ Leetcode/73._Set_Matrix_Zeroes.py
@@ -0,0 +1,23 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        row0 = set()
+        col0 = set()
+
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == 0:
+                    row0.add(i)
+                    col0.add(j)
+
+        for r in row0:
+            matrix[r] = [0] * cols
+
+        for c in col0:
+            for r in range(rows):
+                matrix[r][c] = 0

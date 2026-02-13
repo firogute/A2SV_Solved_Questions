2cc3423e97dab0c9c1fcc6f641aef31f8a006667
Firomsa Guteta <firomsaguteta11@gmail.com>
2026-02-13 11:55:18 +0300

    add implementation of findDiagonalOrder function to return elements of a matrix in diagonal order

---
+++ Leetcode/498._Diagonal_Traverse.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        n = len(mat)
+        m = len(mat[0])
+        res = []
+        row = 0
+        col = 0
+        direction = 1
+
+        while len(res) < n * m:
+            res.append(mat[row][col])
+
+            if direction == 1:
+                if col == m - 1:
+                    row += 1
+                    direction = -1
+                elif row == 0:
+                    col += 1
+                    direction = -1
+                else:
+                    row -= 1
+                    col += 1
+            else:
+                if row == n - 1:
+                    col += 1
+                    direction = 1
+                elif col == 0:
+                    row += 1
+                    direction = 1
+                else:
+                    row += 1
+                    col -= 1
+
+        return res

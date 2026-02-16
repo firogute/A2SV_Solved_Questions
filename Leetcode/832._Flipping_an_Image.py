ca27d36dfada087f7c32251957e401c908d0ab99
Firomsa Guteta <firomsaguteta11@gmail.com>
2026-02-16 13:42:04 +0300

    add implementation of flipAndInvertImage function to flip and invert a binary image

---
+++ Leetcode/832._Flipping_an_Image.py
@@ -0,0 +1,16 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        n = len(image)
+        for i in range(n):
+            temp = image[i][::-1]
+            image[i] = temp
+
+        for i in range(n):
+            for j in range(n):
+                if image[i][j] == 0:
+                    image[i][j] = 1
+                else:
+                    image[i][j] = 0
+
+
+        return image

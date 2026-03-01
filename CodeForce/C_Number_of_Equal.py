5f4a86bd7693979e16d0aeb9a78c74f65f33df18
Firomsa Guteta <firomsaguteta11@gmail.com>
2026-03-02 00:43:12 +0300

    refactor C_Number_of_Equal function to simplify counting logic using Counter

--- CodeForce/C_Number_of_Equal.py
+++ CodeForce/C_Number_of_Equal.py
@@ -1,22 +1,15 @@
+from collections import Counter
+
 n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
-a = [int(x) for x in input().split()]
+A = Counter(a)
+B = Counter(b)
 
-b = [int(x) for x in input().split()]
-
-i = j = 0
 ans = 0
-
-while i < n and j < m:
-    if a[i] == b[j]:
-        t = j
-        while t < m and b[t] == b[j]:
-            ans += 1
-            t += 1
-        i += 1
-    elif a[i] < b[j]:
-        i += 1
-    else:
-        j += 1
+for x in A:
+    if x in B:
+        ans += A[x] * B[x]
 
 print(ans)

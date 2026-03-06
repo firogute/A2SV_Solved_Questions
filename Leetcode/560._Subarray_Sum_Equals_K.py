5562bf55ec1b71b3e875c3ff898a6030f6d1292e
Firomsa Guteta <firomsaguteta11@gmail.com>
2026-03-06 21:18:48 +0300

    add implementation of 560_Subarray_Sum_Equals_K function to count subarrays that sum to k

---
+++ Leetcode/560._Subarray_Sum_Equals_K.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix_sums = defaultdict(int)
+        prefix_sums[0] = 1
+        current_sum = 0
+        count = 0
+
+        for num in nums:
+            current_sum += num
+            diff = current_sum - k
+            count += prefix_sums[diff]
+            prefix_sums[current_sum] += 1
+
+        return count
